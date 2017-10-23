33bd6ca9342b8a63fdf7c4a686e76f1cc04cd279
Cyrille Francois <francois.cyrille@gmail.com>
2017-10-23 20:10:15 +0200

    Delete IT test directory

--- src/main/java/fr/cnes/sonarqube/plugins/framac/report/ReportInterface.java
+++
@@ -1,9 +0,0 @@
-package fr.cnes.sonarqube.plugins.framac.report;
-
-public interface ReportInterface {
-	
-	ReportModuleRuleInterface getModuleCyclomaticMeasure();
-
-	ReportFunctionRuleInterface[] getCyclomaticMeasureByFunction();
-
-}
