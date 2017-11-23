cf298f45f4e36eae219f90cddddcf394a8e099d3
CfrancCyrille <francois.cyrille@gmail.com>
2017-11-23 22:36:53 +0100

    BulletChart component

---
+++ src/main/js/components/MetricsBulletChart/BulletGraph/BulletGraph.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import HorizontalBulletGraph from './HorizontalBulletGraph';
+
+// PropTypes is a separate package now:
+import PropTypes from 'prop-types';
+
+class BulletGraph extends React.Component {
+
+    // Use static properties for propTypes/defaultProps
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        textLabel: PropTypes.string.isRequired,
+        scaleMin: PropTypes.number.isRequired,
+        scaleMax: PropTypes.number.isRequired,
+        performanceVal: PropTypes.number,
+        symbolMarker: PropTypes.number,
+        badVal: PropTypes.number,
+        satisfactoryVal: PropTypes.number,
+        unitsPrefix: PropTypes.string,
+        unitsSuffix: PropTypes.string,
+        titleStyle: PropTypes.string,
+        textFont: PropTypes.string,
+        badColor: PropTypes.string,
+        satisfactoryColor: PropTypes.string,
+        goodColor: PropTypes.string,
+        height: PropTypes.number,
+        width: PropTypes.number
+    }
+
+    static defaultProps = {
+        badColor: "#999999",
+        satisfactoryColor: "#bbbbbb",
+        goodColor: "#dddddd",
+        width: 500,
+        height: 38
+    }
+
+    render() {
+        return ( <
+            div className = "BulletGraph" >
+            <
+            HorizontalBulletGraph title = { this.props.title }
+            textLabel = { this.props.textLabel }
+            scaleMin = { this.props.scaleMin }
+            scaleMax = { this.props.scaleMax }
+            performanceVal = { this.props.performanceVal }
+            symbolMarker = { this.props.symbolMarker }
+            badVal = { this.props.badVal }
+            satisfactoryVal = { this.props.satisfactoryVal }
+            unitsSuffix = { this.props.unitsSuffix }
+            unitsPrefix = { this.props.unitsPrefix }
+            titleStyle = { this.props.titleStyle }
+            textFont = { this.props.textFont }
+            badColor = { this.props.badColor }
+            satisfactoryColor = { this.props.satisfactoryColor }
+            goodColor = { this.props.goodColor }
+            height = { this.props.height }
+            width = { this.props.width }
+            /> < /
+            div >
+        );
+    }
+};
+
+export default BulletGraph;
--- src/main/js/components/MetricsBulletChart/MetricsBulletChart.js
+++ src/main/js/components/MetricsBulletChart/MetricsBulletChart.js
@@ -1,14 +1,71 @@
-import React    from "react";
-import "./MetricsBulletChart.css";
+import React from "react";
+import BulletGraph from './BulletGraph/BulletGraph';
+
+// PropTypes is a separate package now:
+import PropTypes from 'prop-types';
 
 class MetricsBulletChart extends React.Component {
-  render() {
-	  return (
-			    <div className="metrics-bullet-chart">
-			      <h1>MetricsBulletChart</h1>
-			    </div>
-			  );
-  }
+    // Use static properties for propTypes/defaultProps
+    static propTypes = {
+        mocks: PropTypes.array
+    }
+
+    static defaultProps = {
+        mocks: []
+    }
+
+    // Initialize state right in the class body,
+    // with a property initializer:
+    state = {
+        mocks: [{
+            title: "Complexity Simp. ",
+            textLabel: "values",
+            scaleMin: 0,
+            scaleMax: 58,
+            performanceVal: 33.6,
+            symbolMarker: 33.6,
+            badVal: 26,
+            satisfactoryVal: 58,
+            width: 600
+        }, ]
+    }
+
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            div className = "Container" >
+
+            {
+                this.state.mocks.map((graph, i) => {
+                    return ( <
+                        BulletGraph className = "BulletGraph"
+                        key = { i }
+                        title = { graph.title }
+                        textLabel = { graph.textLabel }
+                        scaleMin = { graph.scaleMin }
+                        scaleMax = { graph.scaleMax }
+                        performanceVal = { graph.performanceVal }
+                        symbolMarker = { graph.symbolMarker }
+                        badVal = { graph.badVal }
+                        satisfactoryVal = { graph.satisfactoryVal }
+                        unitsSuffix = { graph.unitsSuffix }
+                        unitsPrefix = { graph.unitsPrefix }
+                        height = { graph.height }
+                        width = { graph.width }
+                        />
+                    )
+                })
+            }
+
+            <
+            /div>
+
+            <
+            /div>
+        );
+    }
+
 }
 
 export default MetricsBulletChart;
