4d4fa296c38e8bbb48bb8b31584359e8b2823fc1
Benoît Garçon <benoit.garcon@outlook.com>
2018-09-18 15:06:44 +0200

    Fix #34 #35 #36

---
+++ src/test/java/fr/cnes/sonar/plugins/icode/exceptions/ICodeExceptionTest.java
@@ -0,0 +1,33 @@
+/*
+ * This file is part of sonar-icode-cnes-plugin.
+ *
+ * sonar-icode-cnes-plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sonar-icode-cnes-plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sonar-icode-cnes-plugin.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.sonar.plugins.icode.exceptions;
+
+import org.junit.Assert;
+
+public class ICodeExceptionTest {
+
+	final String message = "This is a text message.";
+	final ICodeException exception = new ICodeException(message);
+
+	public void test_exception_is_reliable() {
+		try {
+			throw exception;
+		} catch (ICodeException e) {
+			Assert.assertEquals(message, e.getMessage());
+		}
+	}
+}
